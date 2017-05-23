1646861e63b82b134fdfb23df29689c8cf59bd99
Massimiliano Salerno <imafa82@gmail.com>
2017-05-23 15:17:37 +0200

    Recupero parametri get e query

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,37 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        echo $request->getBaseUrl();
+        echo $request->getRequestUri();
+        echo  $request->getMethod();
+        echo $request->getDefaultLocale();
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+    
+    /**
+     * @Route("/{nome}", name="test")
+     */
+    public function testAction(Request $request, $nome)
+    {
+        echo $nome;
+        echo "<br>";
+        echo $request->get('nome');
+        echo "<br>";
+        echo $request->query->get('citta');
+        echo "<br>";
+        echo $request->get('citta');
+        echo "<br>";
+        echo $request->server->get('QUERY_STRING');
+        echo "<br>";
+        echo $request->attributes->get('nome');
+        echo "<br>";
+        echo $request->attributes->get('_route_params')['nome'];
+        // replace this example code with whatever you need
+        return $this->render('default/nome.html.twig', [
+            'nome' => $nome
         ]);
     }
 }
