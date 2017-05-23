df1aab4aa0d05df953aee54451300087a08f06a4
Massimiliano Salerno <imafa82@gmail.com>
2017-05-23 23:42:24 +0200

    Response e Cookie

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class DefaultController extends Controller
 {
@@ -94,5 +96,31 @@ class DefaultController extends Controller
         return $this->render('default/request.html.twig', [
             'nome' => $nome, 'res' => $request
         ]);
+    }
+    /**
+     * @Route("/response", name="response")
+     */
+    public function responseAction(Request $request)
+    {
+        //$response = new Response('Contenuto', 200);
+        //$response->send();
+        $response = new Response();
+        $response->setContent($this->render(':default:response.html.twig'));
+        $response->setStatusCode(404);
+        $response->setCharset('UTF-8');
+        return $response;
+        return $this->render('default/response.html.twig', array(), $response);
+    }
+    
+    /**
+     * @Route("/cookies", name="cookies")
+     */
+    public function cookiesAction(Request $request)
+    {
+        //Istanzio l'oggetto cookie
+        $cookie = new Cookie('nomeCookie', 'ValoreCookie',(time()*3600*27*7));
+        $response = new Response();
+        $response->headers->setCookie($cookie);
+        return $this->render('default/response.html.twig', [] , $response);
     }
 }
